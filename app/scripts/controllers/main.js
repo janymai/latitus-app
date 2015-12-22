00fe610550fe6df52bf56b3d83977a686912bff1
My Mai <mymai@Mys-MacBook-Pro.local>
2015-12-22 22:36:20 +0700

    init structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lachitusAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lachitusAppApp
+ */
+angular.module('lachitusAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
